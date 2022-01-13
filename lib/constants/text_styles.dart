533a1e7d57f30b335815fdb7eaed5ae76e5dabe8
Todd Nelson <tello_nii@outlook.com>
2022-01-13 03:34:32 +0000

    created styling file to style texts

---
+++ lib/constants/text_styles.dart
@@ -0,0 +1,23 @@
+import 'package:finance_app/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+TextStyle buttonLabelStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  fontFamily: "Oxygen",
+  color: textColor,
+);
+
+TextStyle labelStyle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  fontFamily: "Oxygen",
+  color: textColor,
+);
+
+TextStyle headerLabelStyle = TextStyle(
+  fontSize: 36,
+  fontWeight: FontWeight.w700,
+  fontFamily: "ProximaNova",
+  color: textColor,
+);
