f8c5d3ab6d32e42cc35951997940f71272231fdc
Todd Nelson <tello_nii@outlook.com>
2022-01-13 14:39:13 +0000

    added default colours

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 Color primaryColor = const Color(0xFF000000);
 Color buttonColor = const Color(0xFF6527F9);
 Color textColor = const Color(0xFFFFFFFF);
+Color secondaryTextColor = const Color(0xFFBBBBBB);
+Color containerPrimaryColor = const Color(0xFF333333);
