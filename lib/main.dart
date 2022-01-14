55855c93ad0a370a2a3071d5ebb4925fcb2e9c10
Todd Nelson <tello_nii@outlook.com>
2022-01-14 03:40:49 +0000

    added more routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:finance_app/constants/colors.dart';
+import 'package:finance_app/screens/cards_screen.dart';
 import 'package:finance_app/screens/home_screen.dart';
 import 'package:finance_app/screens/splash_screen.dart';
+import 'package:finance_app/screens/user_profile_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,6 +23,8 @@ class MyApp extends StatelessWidget {
       routes: {
         SplashScreen.id: (context) => const SplashScreen(),
         HomeScreen.id: (context) => const HomeScreen(),
+        CardsScreen.id: (context) => const CardsScreen(),
+        UserProfileScreen.id: (context) => const UserProfileScreen(),
       },
       initialRoute: SplashScreen.id,
     );
